7385d5829ddfce499b69019231b32316b17e3d7a
busyok-bedlam <busyokbedlam.gmail.com>
2018-05-05 20:08:13 +0300

    5 comm

--- task7/main.js
+++ task7/main.js
@@ -5,23 +5,19 @@ var opt = {
     itemElement: 'li',
     arrOfItems: ['1', '2', '3', '4', '5']
 }
-
-
-const createElemWithClass = (tag,...classes) => {
+const createElemWithClass = (tag, ...classes) => {
     const elem = document.createElement(tag);
-    for(let item of classes){
+    for (let item of classes) {
         elem.classList.add(item);
     }
     return elem;
 }
-
 class List {
     constructor(destElem, options) {
         this.destElem = destElem;
         this.options = options;
         this.render();
     }
-
     render() {
         this.wrap = document.getElementsByClassName("wrap")[0];
         this.list = document.createElement(this.options.parentElement);
@@ -29,41 +25,45 @@ class List {
         this.add = document.getElementsByClassName("add-button")[0];
         this.remove = document.getElementsByClassName("remove-button")[0];
         this.input = document.getElementsByClassName("field")[0];
-
         for (let i = 0; i < this.options.arrOfItems.length; i++) {
-            const t = document.createElement(this.options.itemElement);
-            t.className = "list-item";
+            const t = createElemWithClass(this.options.itemElement, "list-item");
             t.innerHTML = this.options.arrOfItems[i];
             this.list.appendChild(t);
             this.wrap.appendChild(this.list)
         }
         this.wrap.addEventListener("click", (e) => {
             e.preventDefault();
-            // console.log(e)
             const target = e.target;
+            // addElement handler    
             target.className === "add-button" ? this.addElement() : null
+            // remove element handler
             target.className === "remove-button" && confirm("Are you sure ?") ? this.removeElement() : null
-            target.className.indexOf("list-item") >= 0 ? this.checkElement(target) : null
-            e.ctrlKey === true ? alert("CTRL KEY + TRUE") : null
+            // single check elem
+            target.className.indexOf("list-item") >= 0 
+                && e.ctrlKey === true 
+                && target.className.indexOf("checked") < 0
+                ? (() => {
+                    this.checkElement(target);
+                })() : null;
+                //  ( () => {
+                //     this.resetClasses();
+                // })()
+            // ctrl check elem
+            target.className.indexOf("list-item") >= 0 && e.ctrlKey === false ? (() => {
+                this.resetClasses();
+                this.checkElement(target)
+            })() : null;
+            this.wrap.addEventListener("mousemove", e => {
+                e.shiftKey === true ? alert(" MOUSEMOVE + SHIFTKEY") : null;
+            })
         })
         this.wrap.addEventListener("keydown", e => {
-            // console.log(e.target);
-            console.log(e.keyCode)
+            console.log(e.keyCode);
             const target = e.target;
             target.className === "field" && e.keyCode === 13 ? this.addElement() : null
-            
+
         })
-        // window.addEventListener("keydown", (e) => {
-        //     e.preventDefault();
-        //     console.log(e.target);
-        //     console.log(e.keyCode);
-        //     e.ctrlKey === true ? (() => {
-        //         this.wrap.addEventListener("click",(e) => {
-        //             e.preventDefault()
-        //             alert(e.target)
-        //         })
-        //     })() : null;
-        // })
+
     }
     addElement() {
         if (this.input.value !== "") {
@@ -75,21 +75,25 @@ class List {
             console.log("ADD ELEMENT")
         }
     }
+    resetClasses() {
+        const liArray = [...this.list.children];
+        for (let item of liArray) {
+            item.className = "list-item";
+        }
+    }
     removeElement() {
         const liArray = [...this.list.children];
-        const length = liArray.length;
-        liArray.forEach(item => {
+        for (let item of liArray) {
             item.className.indexOf("checked") >= 0 ? item.remove() : null
-        })
-        console.log("REMOVE ELEMENT")
-
+        }
+        // console.log("REMOVE ELEMENT")
     }
     checkElement(current) {
         const nameOfClass = current.className;
         const checked = nameOfClass.indexOf("checked") >= 0;
-        const listItem = nameOfClass.indexOf("list-item") >=0;        
-        checked && listItem ? current.classList.remove("checked") : 
-        current.classList.add("checked")
+        const listItem = nameOfClass.indexOf("list-item") >= 0;
+        checked && listItem ? current.classList.remove("checked") :
+            current.classList.add("checked")
     }
 
 }
@@ -97,16 +101,16 @@ class List {
 const renderInitialPage = () => {
     let block = document.createDocumentFragment();
 
-    let wrap = createElemWithClass('div','wrap','dggg');
+    let wrap = createElemWithClass('div', 'wrap', 'dggg');
 
-    let input = createElemWithClass('input','field')
+    let input = createElemWithClass('input', 'field')
     input.setAttribute("type", "text");
     input.setAttribute("placeholder", "hello");
-    let addButton = createElemWithClass("span","add-button");
+    let addButton = createElemWithClass("span", "add-button");
     addButton.innerText = "ADD";
     addButton.setAttribute("type", "button");
 
-    let removeButton = createElemWithClass("span","remove-button");
+    let removeButton = createElemWithClass("span", "remove-button");
     removeButton.innerText = "DEL";
     removeButton.setAttribute("type", "button");
 
